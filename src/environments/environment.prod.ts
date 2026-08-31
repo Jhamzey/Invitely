80f818bd3d6c7da9e173cb403ec98812e9e62562
Jhamzey <Bellowjamiu.f@gmail.com>
2026-08-31 11:13:52 +0100

    Quick commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://invitely-backend.onrender.com/api',
+  //apiUrl: 'https://invitely-backend.onrender.com/api',,
+  apiUrl: 'https://invitely-backend.onrender.com',
   googleMapsKey: 'AIzaSyCp0LraGBXFFkqHpShmJ6WauK5_nOUGsyk'
 };
