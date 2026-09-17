ba31dabc970bea38c461039a6ada9b79e3387712
Olabanji Ojo <emmanuelojo291@gmail.com>
2026-09-17 01:04:02 -0700

    chore(env): add Paystack public test key in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://invitely-backend.onrender.com/api',
   siteUrl: 'https://invitely-eight-nu.vercel.app',
   googleMapsKey: 'AIzaSyCp0LraGBXFFkqHpShmJ6WauK5_nOUGsyk',
-  paystackPublicKey: 'pk_test_xxxxxxxxxxxx',
+  paystackPublicKey: 'pk_test_2c0505115dfb9ef47b3b47afb0605d65afffed7c',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'http://localhost:4000/api',
   siteUrl: 'http://localhost:4200',
   googleMapsKey: 'AIzaSyCp0LraGBXFFkqHpShmJ6WauK5_nOUGsyk',
-  paystackPublicKey: 'pk_test_xxxxxxxxxxxx',
+  paystackPublicKey: 'pk_test_2c0505115dfb9ef47b3b47afb0605d65afffed7c',
 };
